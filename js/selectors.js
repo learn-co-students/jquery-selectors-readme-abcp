ff23d8293ea38d3ed68ee2d4e49876e4bdd13cca
Aswathylal Mohanlal <lal.aswathy@gmail.com>
2018-01-27 16:11:09 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
 // declare your functions here...
+function paragraphSelector(){
+return $('p');
+}
+
+function lastImageSelector(){
+  return $('div img:last');
+}
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+
+function divSelector(){
+  return $('div:lt(2)');
+}
+
+function  firstListItem(){
+  return $('ul li:first');
+}
