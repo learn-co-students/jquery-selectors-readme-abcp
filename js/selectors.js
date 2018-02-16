98468bf6a4d09df9bf74333f9434269fccae7065
Inomzhon Ibragimov <mail4inom@gmail.com>
2018-02-16 00:04:25 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector(){
+  var pTags = $("p");
+  return pTags;
+}
+
+function lastImageSelector(){
+  var lastImg = $("img:last");
+  return lastImg;
+}
+
+function ninjaBabySelector(){
+  var ninjaBaby = $("#baby-ninja");
+  return ninjaBaby;
+}
+
+function divSelector(){
+  var divSelector = $(".pics");
+  return divSelector;
+}
+
+function firstListItem(){
+  var firstListItem = $("ul li:first-child");
+  return firstListItem;
+}
